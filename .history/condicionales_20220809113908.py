fe1838e9fc82a0f8fa47cab674a9c591ae9f4d62
David <shaggi6e@gmail.com>
2022-08-09 11:47:07 -0500

    condicional if elif

---
+++ .history/condicionales_20220809113908.py
@@ -0,0 +1,8 @@
+from ast import If
+
+
+edad = int(input("escribe tu edad"))
+if edad >= 18:
+    print("eres mayor de edad")
+else:
+    print("eres menor de edad")
---
+++ .history/condicionales_20220809114232.py
@@ -0,0 +1,15 @@
+
+edad = int(input("escribe tu edad "))
+if edad >= 18:
+    print("eres mayor de edad ")
+else:
+    print("eres menor de edad ")
+
+
+numero = int(input("escribe un numero "))
+if numero >5:
+    print("es mayor a 5")
+elif numero ==5:
+    print("es igual a 5")
+else:
+    print("numero es diferente de 5")
