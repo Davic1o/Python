7eb0df1925c8cc8e08904eb228c7e85020d17e38
David <shaggi6e@gmail.com>
2022-08-09 12:49:25 -0500

    programa para numero aleatorio

---
+++ .history/aleatorio_20220809124911.py
@@ -0,0 +1,19 @@
+#Importamos la libreria random para tener nuemro al azar
+import random
+
+def run():
+    #asignamos el numero randomico a una varaible
+    numero_aleatorio = random.randint(1,10)
+    #creamos el condicional 
+    numero_elegido = int(input("Elije un numero del 1 al 10 :"))
+    while numero_elegido != numero_aleatorio:
+        if numero_elegido < numero_aleatorio:
+            print("Busca un numero mas grande ")
+        else:
+            print("Busca un numero mas pequeño ")
+        numero_elegido = int(input("Elije otro numero  :"))
+    print("Ganaste!")
+
+
+if __name__ == "__main__":
+    run()
